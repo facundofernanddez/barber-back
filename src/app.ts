41e9c42c80d1d2314a7ed39b147b2abb297cad83
Alejandro Facundo Fernandez <91287409+facundofernanddez@users.noreply.github.com>
2024-10-30 22:22:51 -0300

    correr app principal

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,15 @@
 import express from "express";
-import prisma from "./db/db";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const PORT = parseInt(process.env.PORT ?? "3000", 10);
 const HOST = process.env.HOST ?? "localhost";
 
 const app = express();
+
+app.use(express.json());
+
+app.listen(PORT, HOST, () => {
+  console.log(`Server running at http://${HOST}:${PORT}`);
+});
